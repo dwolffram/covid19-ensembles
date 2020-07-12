f3fcce28628ce62f6655cc6afb3b1479e3c9122d
dwolffram <daniel-wolffram@gmx.de>
2020-07-12 19:11:16 +0200

    added bump charts

--- evaluation_plots.R
+++ evaluation_plots.R
@@ -1,6 +1,7 @@
 setwd("/home/dwolffram/covid19-ensembles")
 
-results = read_csv('results/results2.csv')
+results = read.csv('results/results3.csv',
+                   colClasses = c(window_size = "factor", test_date = "Date"))
 
 results_long <- results %>% 
   pivot_longer(names(results)[3:length(results)], names_to = "model", values_to = "wis")
@@ -30,3 +31,120 @@ plot_comparison("EWA")
 plot_comparison("V3")
 plot_comparison("GQRA2")
 plot_comparison("V2")
+
+ggplot(data = results_long, aes(x = model, y = wis)) +
+  #facet_wrap(~window_size) +
+  geom_boxplot(outlier.shape=NA) +
+  ylim(0, 100)
+
+ggplot(data = results_long, aes(x = window_size, y = wis)) +
+  facet_wrap(~model) +
+  geom_boxplot(outlier.shape=NA) +
+  ylim(0, 100)
+
+ggsave('plots/boxplot_windowSizes.png', device='png')
+
+#### Bump Chart
+
+my_theme <- function() {
+  
+  # Colors
+  color.background = "white"
+  color.text = "#22211d"
+  
+  # Begin construction of chart
+  theme_bw(base_size=15) +
+    
+    # Format background colors
+    theme(panel.background = element_rect(fill=color.background, color=color.background)) +
+    theme(plot.background  = element_rect(fill=color.background, color=color.background)) +
+    theme(panel.border     = element_rect(color=color.background)) +
+    theme(strip.background = element_rect(fill=color.background, color=color.background)) +
+    
+    # Format the grid
+    theme(panel.grid.major.y = element_blank()) +
+    theme(panel.grid.minor.y = element_blank()) +
+    theme(axis.ticks       = element_blank()) +
+    
+    # Format the legend
+    theme(legend.position = "none") +
+    
+    # Format title and axis labels
+    theme(plot.title       = element_text(color=color.text, size=20, face = "bold")) +
+    theme(axis.title.x     = element_text(size=14, color="black", face = "bold")) +
+    theme(axis.title.y     = element_text(size=14, color="black", face = "bold", vjust=1.25)) +
+    theme(axis.text.x      = element_text(size=10, vjust=0.5, hjust=0.5, color = color.text)) +
+    theme(axis.text.y      = element_text(size=10, color = color.text)) +
+    theme(strip.text       = element_text(face = "bold")) +
+    
+    # Plot margins
+    theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
+}
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+
+df_rank <- results_long %>% 
+  group_by(window_size, test_date) %>%
+  arrange(window_size, test_date, wis) %>% 
+  mutate(ranking = row_number())
+
+
+bump_chart <- function(windowSize="1", highlight_models=unique(df_rank$model)){
+  df_temp <- subset(df_rank, window_size==windowSize)
+  dates <- sort(unique(df_temp$test_date))
+  
+  df_temp <- df_temp %>%
+    mutate(flag = ifelse(model %in% highlight_models, TRUE, FALSE),
+           model_col = if_else(flag == TRUE, model, "zzz"))
+  
+  pal <- gg_color_hue(length(unique(df_temp$model_col)) -1)
+  pal[length(pal)+1] <- "grey"
+  
+  ggplot(data = df_temp, aes(x = test_date, y = ranking, group = model)) +
+    geom_line(aes(color = model_col, alpha = 1), size = 2) +
+    geom_point(color = "#FFFFFF", size = 4) +
+    geom_point(aes(color = model_col, alpha = 1), size = 4) +
+    geom_point(color = "#FFFFFF", size = 1) +
+    scale_y_reverse(breaks = 1:nrow(df_temp)) +
+    scale_x_date(breaks = unique(df_temp$test_date), 
+                       minor_breaks = unique(df_temp$test_date), expand = c(.05, .05)) +
+    geom_text(data = df_temp %>% filter(test_date == dates[1]),
+              aes(label = model, x = dates[1]-1) , hjust = 1, fontface = "bold", 
+              color = "#888888", size = 4) +
+    geom_text(data = df_temp %>% filter(test_date == dates[length(dates)]),
+              aes(label = model, x = dates[length(dates)]+1) , hjust = 0, fontface = "bold", 
+              color = "#888888", size = 4) +
+    coord_cartesian(xlim = c(dates[1]-4,dates[length(dates)]+4)) +
+    theme(legend.position = "none") +
+    labs(x = "Test Date",
+         y = "Rank",
+         title = "Comparison of Ensemble Methods",
+         subtitle = paste("For Window Size", windowSize, "(Ranked by WIS)")) +
+    my_theme() +
+    scale_color_manual(values = pal)
+}
+
+bump_chart()
+bump_chart("4", c("EWA","V3","QRA3", "GQRA3"))
+
+bump_chart(1)
+ggsave('plots/bump_chart_ws1.png', device='png')
+bump_chart(2)
+ggsave('plots/bump_chart_ws2.png', device='png')
+bump_chart(3)
+ggsave('plots/bump_chart_ws3.png', device='png')
+bump_chart(4)
+ggsave('plots/bump_chart_ws4.png', device='png')
+
+bump_chart(4, c("EWA","V3","QRA3", "GQRA3"))
+
+bump_chart(4, c("EWA"))
+ggsave('plots/bump_chart_EWA.png', device='png')
+
+
+
+
