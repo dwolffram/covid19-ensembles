38a61a6743c9e869393db7d9ece82b98d178b86e
dwolffram <daniel-wolffram@gmx.de>
2020-11-03 12:32:47 +0100

    get relevant models

--- data_exploration.R
+++ data_exploration.R
@@ -1,96 +1,12 @@
 setwd("/home/dwolffram/covid19-ensembles")
 source("data_loading.R")
-
 library(ggplot2)
 
 df <- load_forecasts()
 
-# we need to check if there are missing forecasts over time, we only consider models without gaps
-max_diff_target_end_date <- df %>%
-  #filter(target_end_date >= "2020-05-30") %>%
-  group_by(target, model) %>%
-  summarize(max_diff_target_end_date = max(diff.Date(target_end_date)))
 
-# next we check how many forecasts are available for all models without gaps
-model_availability <- df %>%
-  group_by(target, model) %>%
-  filter(max(diff.Date(target_end_date)) == 7) %>%
-  summarize(count = length(unique(target_end_date)), 
-            start = min(target_end_date), end = max(target_end_date))
-
-relevant_models <- model_availability %>%
-  filter(count > 20)
-
-model_locations <- df %>% 
-  group_by(target, model, target_end_date) %>%
-  summarize(locations = length(unique(location)))
-
-all_states_available <- df %>% 
-  group_by(target, model, target_end_date) %>%
-  summarize(locations = length(unique(location))) %>%
-  group_by(model) %>%
-  filter(min(locations) >= 30)
-
-get_available_models <- function(df, target="1 wk ahead cum death", exclude_gaps=FALSE){
-  # pick target
-  temp <- df %>%
-    filter(target == !!target)
-  
-  # exclude models with missing forecasts over time
-  if (exclude_gaps==TRUE){
-    temp <- temp %>%
-      group_by(model) %>%
-      filter(max(diff.Date(target_end_date)) == 7)
-  }
-  
-  # all states available
-  temp <- temp %>% 
-    group_by(model, target_end_date) %>%
-    summarize(locations = length(unique(location))) %>%
-    group_by(model) %>%
-    filter(min(locations) >= 50)
-  
-  return(temp)
-}
-
-available_models <- get_available_models(df, exclude_gaps=TRUE)
-
-available_models %>%
-  summarize(count = length(unique(target_end_date)), 
-            start = min(target_end_date), end = max(target_end_date))
-relevant_models <- available_models %>%
-  filter(count > 20)
-
-a <- available_models %>%
-  group_by(target_end_date) %>%
-  summarize(count = length(unique(model))) 
-
-temp <- df %>%
-  filter(target == "1 wk ahead cum death")
-
-# all states available
-temp <- temp %>% 
-  group_by(model, target_end_date) %>%
-  summarize(locations = length(unique(location)))
-
-temp <- temp %>%
-  filter(locations >= 50)
-
-temp <- temp %>%
-  group_by(model) %>%
-  summarize(count = length(unique(target_end_date)), 
-            start = min(target_end_date), end = max(target_end_date))
-
-temp <- temp %>% 
-  group_by(model) %>%
-  complete(target_end_date = seq.Date(min(target_end_date), max(target_end_date), by = "week"))
-temp$forecast_missing <- -1*(temp$locations < 50)
-temp <- temp %>% replace_na(list(forecast_missing = 1))
-
-temp$forecast_missing <- 1*is.na(temp$locations)
-
-plot_availability <- function(df, target="1 wk ahead cum death", exclude_gaps=FALSE, 
-                              min_no_locations=50, drop_incomplete=FALSE){
+get_available_models <- function(df, target="1 wk ahead cum death", exclude_gaps=FALSE, 
+                                 min_no_locations=50, drop_incomplete=FALSE){
   # pick target
   temp <- df %>%
     filter(target == !!target)
@@ -125,8 +41,18 @@ plot_availability <- function(df, target="1 wk ahead cum death", exclude_gaps=FA
   if (exclude_gaps==TRUE){
     temp <- temp %>%
       group_by(model) %>%
-      filter(all(forecast_missing == 0))
+      filter(all(forecast_missing != 1))
   }
+  
+  return(temp)
+}
+
+
+plot_availability <- function(df, target="1 wk ahead cum death", exclude_gaps=FALSE, 
+                              min_no_locations=50, drop_incomplete=FALSE){
+  
+  temp <- get_available_models(df, target=target, exclude_gaps=exclude_gaps, 
+                                   min_no_locations=min_no_locations, drop_incomplete=drop_incomplete)
   
   # sort by number of available forecasts per model
   temp <- temp %>%
@@ -150,4 +76,18 @@ plot_availability(df, target = "1 wk ahead cum death", drop_incomplete = TRUE)
 plot_availability(df, target = "1 wk ahead cum death", drop_incomplete = TRUE, exclude_gaps = TRUE)
 
 plot_availability(df, target="4 wk ahead cum death")
-plot_availability(df, target="4 wk ahead cum death", exclude_gaps = TRUE)
+plot_availability(df, target="4 wk ahead cum death", drop_incomplete = TRUE, exclude_gaps = TRUE)
+
+
+available_models <- get_available_models(df, target="1 wk ahead cum death", 
+                                         drop_incomplete=TRUE, exclude_gaps=TRUE)
+
+relevant_models <- available_models %>%
+  summarize(count = length(unique(target_end_date)), 
+            start = min(target_end_date), end = max(target_end_date))
+
+relevant_models <- relevant_models %>%
+  filter(start <= "2020-05-23") %>%
+  pull(model)
+
+relevant_models
