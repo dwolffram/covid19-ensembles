70a43ced263742b895645930d0212702bc579dec
dwolffram <daniel-wolffram@gmx.de>
2020-11-04 22:43:19 +0100

    refactor ensemble building

--- ensemble_functions.R
+++ ensemble_functions.R
@@ -1,3 +1,4 @@
+source("ensemble_methods.R")
 options(dplyr.summarise.inform=F)
 
 sort_quantiles <- function(df_forecast){
@@ -40,9 +41,9 @@ train_test_split <- function(df, test_date, horizon, window_size){
   return(list(df_train=df_train, df_test=df_test))
 }
 
-# a <- train_test_split(df, test_date=as.Date("2020-08-22"), horizon=1, window_size=4)
-# a1 <- a$df_train
-# a2 <- a$df_test
+dfs <- train_test_split(df, test_date=as.Date("2020-08-22"), horizon=1, window_size=4)
+df_train <- dfs$df_train
+df_test <- dfs$df_test
 
 
 ensemble_forecasts <- function(df, window_sizes, ensembles=c("EWA")){
@@ -108,9 +109,77 @@ ensemble_forecasts <- function(df, window_sizes, ensembles=c("EWA")){
 
 possible_dates <- unique(df$target_end_date)
 
-build_ensembles <- function(df_train, df_test, ensembles=c("EWA")){
+build_ensembles <- function(df_train, df_test, 
+                            ensembles=c("EWA", "MED", "V2", "V3", "V4", 
+                                        "QRA2", "QRA3", "QRA4", 
+                                        "GQRA2", "GQRA3", "GQRA4")){
+  df_ensembles <- data.frame()
+
+  for (ensemble in ensembles){
+    print(ensemble)
+    switch (ensemble,
+            "EWA" = {
+              df_forecast <- EWA(df_test)
+            },
+            "MED" = {
+              df_forecast <- MED(df_test)
+            },
+            "V2" = {
+              p <- v2_fit(df_train)
+              df_forecast <- V2(df_test, params=p)
+            },
+            "V3" = {
+              p <- v3_fit(df_train)
+              df_forecast <- V3(df_test, params=p)
+            },
+            "V4" = {
+              p <- v4_fit(df_train)
+              df_forecast <- V3(df_test, p$params, p$intercept)
+            },
+            "QRA2" = {
+              p <- qra2_fit(df_train)
+              df_forecast <- QRA2(df_test, params=p)
+            },
+            "QRA3" = {
+              p <- qra3_fit(df_train)
+              df_forecast <- QRA3(df_test, params=p)
+            },
+            "QRA4" = {
+              p <- qra4_fit(df_train)
+              df_forecast <- QRA4(df_test, p$params, p$intercepts)
+            },
+            "GQRA2" = {
+              groups <- get_quantile_groups()
+              p <- gqra2_fit(df_train, groups)
+              df_forecast <- GQRA_3(df_test, groups, p)
+            },
+            "GQRA3" = {
+              groups <- get_quantile_groups()
+              p <- gqra3_fit(df_train, groups)
+              df_forecast <- GQRA_3(df_test, groups, p)
+            },
+            "GQRA4" = {
+              groups <- get_quantile_groups()
+              p <- gqra4_fit(df_train, groups)
+              df_forecast <- GQRA_4(df_test, groups, p$params, p$intercepts)
+            }
+    )
+    df_forecast <- sort_quantiles(df_forecast)
+    df_forecast$model <- ensemble
+    df_ensembles <- bind_rows(df_ensembles, df_forecast)
+  }
   
+  return(df_ensembles)
 }
+
+
+a <- build_ensembles(df_train, df_test, c("EWA", "MED", "V3", "QRA3", "GQRA3"))
+
+
+
+
+
+
 
 df_ensembles <- data.frame()
 ensembles <- c("EWA", "MED", "V2", "V3", "V4", "QRA2", "QRA3", 
@@ -169,3 +238,5 @@ for (ensemble in ensembles){
   df_forecast$model <- ensemble
   df_ensembles <- bind_rows(df_ensembles, df_forecast)
 }
+
+write.csv(df_ensembles, 'results/df_ensembles_test.csv', row.names=FALSE)
