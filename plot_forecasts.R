1ac703949d0a5883a5ed523165a85c2b71554113
dwolffram <daniel-wolffram@gmx.de>
2020-11-16 21:23:00 +0100

    option to center forecasts around truth

--- plot_forecasts.R
+++ plot_forecasts.R
@@ -4,9 +4,6 @@ source("ensemble_methods.R")
 source("ensemble_functions.R")
 
 
-# cols <- colorRampPalette(c("deepskyblue4", "lightgrey"))(length(levels_coverage) + 1)[-1]
-cols <- colorRampPalette(c("deepskyblue4", "lightgrey"))(2 + 1)[-1]
-
 
 df <- load_ensembles("data/ensemble_forecasts/df_ensembles_1wk_2020-11-13.csv", add_baseline = TRUE)
 
@@ -14,19 +11,13 @@ df_wide <- pivot_wider(df, names_from=quantile, names_prefix="value.", values_fr
 df_wide <- add_truth(df_wide)
 df_wide <- add_location_names(df_wide)
 
-# results <- add_truth(df_ensembles)
-
-data <- subset(df_wide, location=="US" & model =="MED" & window_size==4)
-data <- subset(df_wide, location=="34" & window_size==4)
-data <- subset(df_wide, model=="V3" & window_size==4)
-data <- subset(df_wide, location=="US" & window_size==4)
-
 
 plot_forecast <- function(data, models, locations, window_sizes,
-                          facet, facet_row=location, facet_col=model,
-                          incidence=FALSE, title=NULL,
+                          facet, facet_row=location_name, facet_col=model,
+                          incidence=FALSE, center=FALSE, title=NULL,
                           start_date='1900-01-01', end_date='3000-01-01',
                           ncol=4, dir='v', scales='fixed'){
+  cols <- colorRampPalette(c("deepskyblue4", "lightgrey"))(2 + 1)[-1] # length(levels_coverage) + 1
   facet <- ensym(facet)
   
   if(missing(models)){
@@ -37,7 +28,7 @@ plot_forecast <- function(data, models, locations, window_sizes,
     locations <- unique(data$location)
   }
   
-  if(missing(window_sizes)){
+  if(missing(window_sizes) & 'window_size' %in% colnames(data)){
     window_sizes <- unique(data$window_size)
   }
   
@@ -45,10 +36,14 @@ plot_forecast <- function(data, models, locations, window_sizes,
   data <- data %>%
     filter(model %in% models,
            location %in%  locations,
-           window_size %in% window_sizes,
            target_end_date >= start_date,
            target_end_date <= end_date
     )
+  
+  if('window_size'  %in% colnames(data)){
+    data <- data %>%
+      filter(window_size %in% window_sizes)
+  }
   
   if (incidence){
     if (missing(facet)){
@@ -58,14 +53,35 @@ plot_forecast <- function(data, models, locations, window_sizes,
       grouping_vars <- facet
     }
     
+    horizon = as.numeric(substr(unique(data$target), 1, 1))
+    
     data <- data %>% 
       group_by(!!!grouping_vars) %>%
-      mutate(value.0.5 = value.0.5 - lag(truth)) %>%
-      mutate(value.0.05 = value.0.05 - lag(truth)) %>%
-      mutate(value.0.95 = value.0.95 - lag(truth)) %>%
-      mutate(value.0.25 = value.0.25 - lag(truth)) %>%
-      mutate(value.0.75 = value.0.75 - lag(truth)) %>%
+      mutate(value.0.5 = value.0.5 - lag(truth, horizon)) %>%
+      mutate(value.0.05 = value.0.05 - lag(truth, horizon)) %>%
+      mutate(value.0.95 = value.0.95 - lag(truth, horizon)) %>%
+      mutate(value.0.25 = value.0.25 - lag(truth, horizon)) %>%
+      mutate(value.0.75 = value.0.75 - lag(truth, horizon)) %>%
       mutate(truth=c(NA, diff(truth))) %>%
+      drop_na()
+  }
+  
+  if (center){
+    if (missing(facet)){
+      grouping_vars <- quos(model, location)
+    }
+    else{
+      grouping_vars <- facet
+    }
+    
+    data <- data %>% 
+      group_by(!!!grouping_vars) %>%
+      mutate(value.0.5 = value.0.5 - truth) %>%
+      mutate(value.0.05 = value.0.05 - truth) %>%
+      mutate(value.0.95 = value.0.95 - truth) %>%
+      mutate(value.0.25 = value.0.25 - truth) %>%
+      mutate(value.0.75 = value.0.75 - truth) %>%
+      mutate(truth=0) %>%
       drop_na()
   }
   
@@ -97,24 +113,26 @@ plot_forecast <- function(data, models, locations, window_sizes,
 
 plot_forecast(df_wide, window_sizes=4, models=c('V4', 'QRA4'),
               locations=c('US', 36), incidence=TRUE,
-              facet_row=location, facet_col=model,
               scales='free_y')
 
 plot_forecast(df_wide, window_sizes=4,
               locations=c('US', 36), incidence=TRUE,
-              facet_row=location, facet_col=model,
+              facet_row=location_name, facet_col=model,
               scales='free_y')
 
-plot_forecast(df_wide, window_sizes=4, models=c('V4', 'QRA4'),
-              incidence=TRUE,
-              facet_row=location, facet_col=model,
-              scales='free_y')
+# plot_forecast(df_wide, window_sizes=4, models=c('V4', 'QRA4'),
+#               incidence=TRUE,
+#               facet_row=location_name, facet_col=model,
+#               scales='free_y')
 
 
 
 
 plot_forecast(df_wide, window_sizes=4, locations='US', models='QRA3', incidence=FALSE, ncol=4)
 plot_forecast(df_wide, window_sizes=4, locations='US', facet=model, incidence=TRUE, ncol=4)
+
+plot_forecast(df_wide, window_sizes=4, models='V4', facet=location_name, incidence=TRUE, 
+              ncol=8, dir='h', scales='free_y')
 
 plot_forecast(df_wide, window_sizes=4, locations=c('US', 36), models='QRA3', 
               facet=location, incidence=TRUE, ncol=4)
@@ -127,28 +145,51 @@ plot_forecast(df_wide, window_sizes=4, locations=c('US', 36), models='QRA3',
 
 
 
-### OLD
+### INDIVIDUAL MODELS
 
-plot_forecast(subset(df_wide, window_size==4 & location=='US'), facet=model, incidence=TRUE, ncol=4)
-plot_forecast(subset(df_wide, window_size==4 & location=='US' & model=='QRA3'), incidence=FALSE, ncol=4)
-plot_forecast(subset(df_wide, window_size==4 & model=='V3'), facet=location_name, scales='free_y', 
-              incidence=TRUE, ncol=8, dir='h')
+df_individual <- load_forecasts(models = c("CovidAnalytics-DELPHI", "COVIDhub-baseline", "CU-select", 
+                                           "JHU_IDD-CovidSP", "LANL-GrowthRate", "MOBS-GLEAM_COVID", 
+                                           "PSI-DRAFT", "UCLA-SuEIR", "UMass-MechBayes", "YYG-ParamSearch"),
+                                targets = "4 wk ahead cum death",
+                                exclude_locations = c("11", "60", "66", "69", "72", "74", "78"), 
+                                start_date = "2020-06-20", 
+                                end_date = "2020-10-10") %>% 
+  select(-c(forecast_date, type)) %>%
+  select(target_end_date, everything())
+
+df_individual <- pivot_wider(df_individual, names_from=quantile, names_prefix="value.", values_from=value)
+df_individual <- add_truth(df_individual)
+df_individual <- add_location_names(df_individual)
+
+plot_forecast(df_individual, locations='US', facet=model, incidence=TRUE, ncol=4)
+
+plot_forecast(df_individual, locations='US', facet=model, center=TRUE,
+              ncol=4, dir='h', scales='free_y')
+
+plot_forecast(df_wide, window_sizes=4, locations='US', facet=model, center=TRUE,
+              ncol=4, dir='v', scales='free_y')
+
+plot_forecast(df_wide, window_sizes=4, locations=27, facet=model, center=TRUE,
+              ncol=4, dir='v', scales='free_y')
+
+plot_forecast(df_individual, locations=27, facet=model, center=TRUE,
+              ncol=4, dir='v', scales='free_y')
+
+plot_forecast(df_individual, models='YYG-ParamSearch', facet=location_name, incidence=TRUE, 
+              ncol=8, dir='h', scales='free_y')
+
+plot_forecast(df_individual, models='UCLA-SuEIR', facet=location_name, incidence=TRUE, 
+              ncol=8, dir='h', scales='free_y')
+
+plot_forecast(df_individual, locations='US', facet=model, incidence=FALSE, ncol=4)
+
+
+unique(df_individual$model)
 
 
 
 
-data <- subset(add_truth(df_ensembles), location=="US")
-data <- subset(individual_results, location=="US")
-data <- subset(results, location=="34" & window_size==4)
 
-data <- subset(individual_results, method=="COVIDhub-baseline")
-data <- subset(individual_results, method=="YYG-ParamSearch")
-
-plot_forecast(data, incidence=FALSE, title='Forecasts for New Jersey')
-
-plot_forecast(subset(df_wide, window_size==4 & location=='US'), incidence=TRUE, 
-              title="1 week ahead individual forecasts with 50%- and 90%-prediction intervals (national level)")
-  #theme(text = element_text(size = 25)) 
 
 plot_forecast(data, incidence=TRUE, 
               title="YYG-ParamSearch forecasts with 50%- and 90%-prediction intervals")
