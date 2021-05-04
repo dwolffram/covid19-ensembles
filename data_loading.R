75ecab206f44324fa1c78e0e31d910730c514daf
dwolffram <daniel-wolffram@gmx.de>
2021-05-04 17:31:41 +0200

    load truth from online repo not local clone

--- data_loading.R
+++ data_loading.R
@@ -9,7 +9,7 @@ library(MMWRweek)
 
 load_truth <- function(target='Cumulative Deaths', as_of){
   if(missing(as_of)){
-    truth <- read.csv(paste0(path_hub, 'data-truth/truth-', target, '.csv'),
+    truth <- read.csv(paste0('https://raw.githubusercontent.com/reichlab/covid19-forecast-hub/master/data-truth/truth-', str_replace(target, ' ', '%20'), '.csv'),
                       colClasses = c(location="character", date ="Date")) %>% 
       select(-location_name)
     
